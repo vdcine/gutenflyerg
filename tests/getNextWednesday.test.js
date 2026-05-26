c01fefad6e37abbe946e4a6adae5d633b8be2bae
MeLi-L1 <207492644+MeLi-L1@users.noreply.github.com>
2026-05-26 12:52:14 +0000

    Agregue tests

--- tests/getNextWednesday.test.js
+++ tests/getNextWednesday.test.js
@@ -1,8 +1,21 @@
 import {test,expect} from "bun:test";
 import {getNextWednesday} from "./getNextWednesday.js";
 test ("que devuelva algun valor", () =>{
+
 	expect(getNextWednesday()).toBeDefined();});
 test ("que devuelva un string", () => {
+
 	expect(typeof getNextWednesday()).toBe("string");});
 test ("que contenga 2026", () => {
 	expect(getNextWednesday()).toContain("2026");});
+
+test ("que devuelva dia de la semana", () => {
+	const miercoles= new Date(getNextWednesday());
+	expect(miercoles.getDay()).toBe(3);}); 
+
+test ("que devuelva numero del proximo miercoles", () => {
+	const fecha= new Date(getNextWednesday());
+	expect(fecha.getDate()).toBe(27);});
+
+test ("que devuelva la fecha del proximo miercoles", () => {
+	expect(getNextWednesday()).toBe("2026-05-27");});
